4f5cb910b9b8bb5d02208b3632ab268975c59b95
Bonous <simon.gorbet@gmail.com>
2021-03-26 13:23:48 -0400

    Update prototype_arrays.ino

--- Code/prototype_arrays/prototype_arrays.ino
+++ Code/prototype_arrays/prototype_arrays.ino
@@ -1,10 +1,14 @@
-int timeA;
-int angleA;
-int timeB;
-int angleB;
+int motor1_angles[] = {};
+int motor1_times[] = {};
+
+int time1A;
+int angle1A;
+int time1B;
+int angle1B;
 int timeCurrent;
 int timeSegment;
 int timePrevious;
+int i = 0;
 bool dirHigh;
 
 #define slope ((angleB - angleA) / (timeB - timeA))
@@ -15,12 +19,14 @@ void setup() {
 
 void calculate_angle(float m,int t,int tp){
   float angle = m * (t - tp);
+  stepsDir = (angle > 0)
+  steps = angle / motorAcc
 }
 
 void move_motor(int dir,float stp){
   dirHigh = dir;
-  digitalWrite(DIR_PIN, dir);
-  for(int i = 0; i < abs(stp); i++){
+  digitalWrite(DIR_PIN, stepsDir);
+  for(int i = 0; i < (int)abs(stp); i++){
     digitalWrite(STEP_PIN, HIGH);
     delay(1);
     digitalWrite(STEP_PIN, LOW);
@@ -28,10 +34,10 @@ void move_motor(int dir,float stp){
 }
 
 void loop() {
-  timeA = __;
-  timeB = __;
-  angleA = __;
-  angleB = __;
+  time1A = motor1_time[i];
+  time1B = motor1_time[i+1];
+  angle1A = motor1_angle[i];
+  angle1B = motor1_angle[i+1];
 
   while ((timeB - timeCurrent) >= 0){
     timeCurrent = millis();
@@ -39,5 +45,6 @@ void loop() {
     timePrevious = timeCurrent;
     
   }
-  
+
+  i++;
 }
