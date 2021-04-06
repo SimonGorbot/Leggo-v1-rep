6ac61f09f4dbb437ea2e69bd59744a3d1ecae945
Bonous <79294077+Bonous@users.noreply.github.com>
2021-04-06 09:07:41 -0400

    update arrays
    having issues with walking

--- Code/Array_test_1/Array_test_1.ino
+++ Code/Array_test_1/Array_test_1.ino
@@ -7,8 +7,11 @@
 
 int motor1_angle[num_points] = {49,52,54,57,73,83,87,89,90,85,70,57,40,32,35,43,49};
 long motor1_time[num_points] = {0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3200};
+//long motor1_time[num_points] = {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000};
 int motor2_angle[num_points] = {49,43,35,32,40,57,70,85,90,89,87,83,73,57,54,52,49};
 long motor2_time[num_points] = {0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3200};
+//long motor2_time[num_points] = {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000};
+
 
 long timeCurrent;
 long timePrevious1;
@@ -17,6 +20,7 @@ long number_cycles = 0;
 int steps;
 int stepsDir;
 int i = 0;
+int wait = 20;
 
 #define cycle_time (number_cycles * motor1_time[num_points - 1])
 
@@ -36,6 +40,7 @@ float m2_calculate_angle(float m,int t,int tp){
 
 void m1_move_motor(int dir,float stp){
   digitalWrite(DIR_PIN1, dir);
+  delay(wait);
   for(int i = 0; i < (int)stp; i++){
     digitalWrite(STEP_PIN1, HIGH);
     delay(1);
@@ -46,6 +51,7 @@ void m1_move_motor(int dir,float stp){
 
 void m2_move_motor(int dir,float stp){
   digitalWrite(DIR_PIN2, dir);
+  delay(wait);
   for(int i = 0; i < (int)stp; i++){
     digitalWrite(STEP_PIN2, HIGH);
     delay(1);
@@ -69,6 +75,9 @@ void setup() {
   m1_move_motor((motor1_angle[0] < 0), (motor1_angle[0]/motorAcc));
   m2_move_motor((motor2_angle[0] > 0), (motor2_angle[0]/motorAcc));
   delay(3000);
+  timeCurrent = millis();
+  timePrevious1 = millis();
+  timePrevious2 = millis();
 }
 
 void loop() {
@@ -93,17 +102,20 @@ void loop() {
     if (steps > 0){
       m1_move_motor(stepsDir, steps);
       timePrevious1 = timeCurrent;
+      //timePrevious1 = (steps * motorAcc) / slope1;
+      Serial.print("ONE\t"); Serial.print(steps); Serial.print(" | "); Serial.println(stepsDir);
     }
     m2_calculate_angle(slope2, timeCurrent, timePrevious2);
     if (steps > 0){
       m2_move_motor(stepsDir, steps);
       timePrevious2 = timeCurrent;
-      Serial.println(steps);
+      //timePrevious2 = (steps * motorAcc) / slope2;
+      Serial.print("TWO\t"); Serial.print(steps); Serial.print(" | "); Serial.println(stepsDir);
     }
   }
 
   i++;
-  if (i == 5){
+  if (i == (num_points-1)){
     i = 0;
     timeCurrent = 0;
     timePrevious1 = 0; timePrevious2 = 0;
